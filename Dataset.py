1a1138810444b582a8e558ee8788f4519b5b15a6
swjcpy <swjcpy@hotmail.com>
2019-03-29 11:09:43 -0400

    current stable version

--- Dataset.py
+++ Dataset.py
@@ -10,6 +10,12 @@
     """
 import scipy.sparse as sp
 import numpy as np
+import pdb
+
+num_samples = 6040
+num_genders = 2
+num_jobs = 21
+num_ages = 56
 
 class Dataset(object):
     '''
@@ -20,10 +26,11 @@ class Dataset(object):
         '''
             Constructor
             '''
-        self.trainMatrix = self.load_rating_file_as_matrix(path + "train_data.csv")
-        self.testRatings = self.load_rating_file_as_list(path + "test_data.csv")
-        self.testNegatives = self.load_negative_file(path + "test_negative.csv")
-        self.trainFeature = self.load_train_feature(path + "users.csv")
+        self.trainMatrix = self.load_rating_file_as_matrix(path + ".train.rating")
+        self.testRatings = self.load_rating_file_as_list(path + ".test.rating")
+        self.testNegatives = self.load_negative_file(path + ".test.negative")
+        self.userFeature = self.load_train_feature(path + ".users")
+        self.itemFeature = self.load_item_feature(path + ".movies")
         assert len(self.testRatings) == len(self.testNegatives)
         
         self.num_users, self.num_items = self.trainMatrix.shape
@@ -34,8 +41,8 @@ class Dataset(object):
             line = f.readline()
             # while line != None and line != "":
             while line is not None and line != "":
-                arr = line.strip().split(",")
-                user, item = int(arr[0]), int(float(arr[1]))
+                arr = line.strip().split("\t")
+                user, item = int(arr[0]), int(arr[1])
                 ratingList.append([user, item])
                 line = f.readline()
         return ratingList
@@ -46,8 +53,7 @@ class Dataset(object):
             line = f.readline()
             # while line != None and line != "":
             while line is not None and line != "":
-                line = line.split('"')[1]
-                arr = line.strip().split(",")
+                arr = line.strip().split("\t")
                 negatives = []
                 for x in arr[:]:
                     negatives.append(int(x))
@@ -66,8 +72,8 @@ class Dataset(object):
             line = f.readline()
             # while line != None and line != "":
             while line is not None and line != "":
-                arr = line.strip().split(",")
-                u, i = int(arr[0]), int(float(arr[1]))
+                arr = line.strip().split("\t")
+                u, i = int(arr[0]), int(arr[1])
                 num_users = max(num_users, u)
                 num_items = max(num_items, i)
                 line = f.readline()
@@ -77,32 +83,56 @@ class Dataset(object):
             line = f.readline()
             # while line != None and line != "":
             while line is not None and line != "":
-                arr = line.strip().split(",")
-                user, item = int(arr[0]), int(float(arr[1]))
+                arr = line.strip().split("\t")
+                user, item = int(arr[0]), int(arr[1])
                 mat[user, item] = 1.0
                 line = f.readline()
         return mat
 
-def load_train_feature(self, filename):
-    feature_list = []
-    with open(filename, 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            line = line.strip().split(',')
-            if line[1] == 'M':
-                line[1] = 1
-                elif line[1] == 'F':
-                    line[1] = -1
-            else:
-                line[1] = 0
-                try:
-                    line[2] = int(float(line[2]))
-                    line[3] = int(float(line[3]))
-                    line[4] = int(line[4].split('-')[0])
-            except:
-                line[2] = 0
-                line[3] = 0
-                line[4] = 0
+    def load_train_feature(self, filename):
+        feature_list = []
+        with open(filename, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line.strip().split("\t")
+                line[1] = int(line[1])
+                line[2] = int(line[2])
+                line[3] = int(line[3])
+                line[4] = int(line[4])
                 feature_list.append(line[1:])
 
-    return feature_list
+        return feature_list
+
+    def load_item_feature(self, filename):
+        # build dictionary
+        word_dict = {}
+        max_length = 0
+        sentences = []
+        
+        with open(filename, 'r') as f:
+            lines = f.readlines()
+
+            for line in lines:
+                line = line.strip().split(" ")
+                sentences.append(line)
+                max_length = max(max_length, len(line))
+                for word in line[1:]:
+                    if word not in word_dict:
+                        word_dict[word] = len(word_dict) + 1
+        print("movie dictionary size: ")
+        print(len(word_dict))
+
+        # define feature list
+        feature_list = {}
+        print("max length: ")
+        print(max_length)
+
+        for i, sentence in enumerate(sentences):
+            temp = np.zeros((max_length-1))
+            idx = int(sentence[0])
+            for j, word in enumerate(sentence[1:]):
+                temp[j] = word_dict[word]
+            feature_list[idx] = temp
+
+        return feature_list
+            
